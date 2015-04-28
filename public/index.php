64a26abbb9ee861cbd2f4a664d9cba81d4d504c2
valimms <valentin.ilea@mindmagnetsoftware.com>
2015-04-28 12:46:09 +0300

    upd

--- public/index.php
+++ public/index.php
@@ -19,7 +19,6 @@ if (APPLICATION_ENV == 'development') {
     define('MAIN_DOMAIN', 'http://www.tirbox.ro');
 }
 
-
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
