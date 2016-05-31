e4e0bddcf8b13b6b7c76041af1dd83d1a5fc938f
valimms <valentin.ilea@mindmagnetsoftware.com>
2016-05-31 19:47:51 +0300

    upd

--- public/blog/app/views/article_tmp.php
+++
@@ -1,9 +0,0 @@
-<?php $dateObj = new \DateTime($article["datetime"]); ?>
-<dl style="padding: 10px 0 20px 0">
-    <dt><?php echo $article["title"]; ?></dt>
-    <dd>
-        <?php echo substr($article["content"], 0, 150);?>
-        <a href="index.php?page=articles&action=viewarticle&id=<?=$article['id']?>">view article</a>
-    </dd>
-    <dd><?php echo $dateObj->format('d F, Y, H:i');?></dd>
-</dl>
